5ddf2a4e1999341b20f66d41fd8198a6393ba723
hackghost <higueragiovani@gmail.com>
2020-09-18 02:01:16 -0700

    Actulización de seccion de residencias con imagenes y documentos para descargar.

--- resources/views/content/vinculacion/residencias.blade.php
+++ resources/views/content/vinculacion/residencias.blade.php
@@ -3,7 +3,7 @@
 
 @section('content')
 
-  <a href='residencias.html' style='padding: 1%;'>  </a><section class='noticia' style=' background-color: transparent; border: 0px solid #000;'><article class='titulo' style='text-align:center; font-size: .8vw;'>
+  <a href='residencias.html' style='padding: 1%;'>  </a><article class='titulo' style='text-align:center; font-size: .8vw;'>
       <h2 style='font-weight: bold; font-size: 2.5em; color: #1B396A;'> Residencias Profesionales: </h2>
     </article>
     <article>
@@ -11,16 +11,11 @@
         <img src='{{asset('images/content/vinculacion/residencias/residencia.jpg')}}' style='width:100%; max-width: 600px; margin: 1%;' >
       </a>
     </article>
-    <br /><!--<article style="text-align:right;">
-						<div class="fb-share-button"
-							data-href="http://pruebas.tecnm.mx:8080/portaltecnm/"
-							data-layout="button_count">
-						</div>
-                        <article>--><article style='font-size: 1.5em;'>
+    <br /><article style='font-size: 1.5em;'>
 
 
       <p><h3>¿QUE ES LA RESIDENCIA PROFESIONAL?</h3></p>
-
+    <hr class="red">
       <p align="justify">Se considera residencia profesional a aquella actividad, realizada durante el desarrollo de un proyecto o en la aplicación práctica de un modelo, en cualquiera de las áreas de desarrollo establecidas, que definan una problemática y propongan una solución viable, a través de la participación directa del alumno en desempeños propios de su futura profesión.<br>
 
         Las <strong>Residencias Profesionales</strong> se plantean como un proceso de aprendizaje que implica la aplicación del conocimiento teórico adquirido en las aulas con diversas experiencias prácticas a las cuales propondrá respuesta dentro del ámbito laboral.
@@ -28,14 +23,14 @@
 
 
       <p><h3>DURACION</h3></p>
-
+    <hr class="red">
       <p align="justify">Su duración queda determinada por un período de 4 meses como tiempo mínimo y 6 meses como tiempo máximo, debiendo acumularse un mínimo de 500 horas.
         La asignación de proyectos de residencia profesional, se debe realizar en periodos previos a la elección de la carga académica del inicio al periodo escolar
       </p>
 
 
       <p><h3>REQUISITOS:</h3></p>
-
+    <hr class="red">
       <p align="justify">Los alumnos(as) que soliciten la asignación oficial de su proyecto de residencia profesional, por cualquiera de las opciones antes mencionadas, deberán presentarse en la División de estudios profesionales o área correspondiente, donde entregarán:  <br>
         a) Tener aprobado al menos el 80% de créditos de su plan de estudio.<br>
         b) Tener acreditadas las actividades complementarias.<br>
@@ -43,94 +38,105 @@
         d) No contar con ninguna asignatura en condiciones de “Curso Especial”
       </p>
 
-      <hr>
+    <hr class="red">
 
 
 
       <p><h3> <u>DOCUMENTOS A ENTREGAR AL INICIO DE RESIDENCIAS PROFESIONALES: </u></h3></p>
+    <hr class="red">
       <p align="justify">
 
         <strong>Estructura del reporte</strong> ESTRUCTURA DEL REPORTE PRELIMINAR DE RESIDENCIA PROFESIONAL<br>
        
       <p align="center">
         <a target="_blank" data-toogle="tooltip"  title="Escructura del reporte preliminar de residencia" download href='{{asset('documents/content/vinculacion/residencias/estruc.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
-      <p align="center">Clic en la imagen para descargar documento</p>
       <br>
 
 
-      1.- <strong>Portada.</strong> (Portada para el trabajo final de residencias profesionales) Portada oficial autorizada, tu trabajo debera de llevar esta portada.<br>
+      1.- <strong>Portada.
+      <hr class="red">
+    </strong> (Portada para el trabajo final de residencias profesionales) Portada oficial autorizada, tu trabajo debera de llevar esta portada.<br>
 
+    <p align="center">
+      <a target="_blank" data-toogle="tooltip"  title="Portada de residencias" download href='{{asset('documents/content/vinculacion/residencias/portada_residencia_itschv4.doc')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
+
+    <br>
       <p align="center">
         <a href='vinculacion/portada_residencia_itschv4.doc><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar Portada de residencias' border='0'></a> </p>
-      <p align="center">Clic en la imagen para descargar documento</p>
+
+      <br>
+
+
+      1.1 - <strong>Solicitud de residencias.</strong>
+    <hr class="red">
+    (Periodo bien establecido fecha inicio y fecha final de residencias) entregarlo a la encargada de residencias al inicio de su residencia.<br>
+
+    <p align="center">
+      <a target="_blank" data-toogle="tooltip"  title="Solicitud de residencias " download href='{{asset('documents/content/vinculacion/residencias/solicitud_residencias_2020.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
+    <br>
+
+      2.- <strong>Anteproyecto.</strong>  <hr class="red">firmado de aceptación por el revisor. Entregarlo a la encargada de residencias al inicio de su residencia.<br><br>
+
+      3.- <strong>Convenio de residencias profesinales.</strong>  <hr class="red">(Llenar el convenio y entregarlo a la encargada de residencias y una copia a vinculación).<br>
+
+
+    <p align="center">
+      <a target="_blank" data-toogle="tooltip"  title="Convenio  de residencias " download href='{{asset('documents/content/vinculacion/residencias/cnvdw.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
+    <br>
+
+
+
+      4.- <strong>Cardex con calificaciones.</strong>  <hr class="red">(tener aprobado al menos 80% de créditos de su plan de estudio). Entregarlo a la encargada de residencias al inicio de su residencia.<br>
       <br>
 
 
-      1.1 - <strong>Solicitud de residencias.</strong> (Periodo bien establecido fecha inicio y fecha final de residencias) entregarlo a la encargada de residencias al inicio de su residencia.<br>
+
+      5.- <strong>Carta de aceptación.</strong> <hr class="red"> (Hoja membretada y dirigida al jefe de carrera, debe incluir nombre del alumno, nombre de proyecto, asesor por parte de la empresa, fecha de inicio y fecha final de residencias). Entregarlo a la encargada de residencias al inicio de su residencia.<br>
+
+    <p align="center">
+      <a target="_blank" data-toogle="tooltip"  title="Carta de aceptacion  de residencias " download href='{{asset('documents/content/vinculacion/residencias/carta_aceptacionv1.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
+    <br>
+
+      5.1.- <strong>Carta de terminación.</strong>  <hr class="red">(Hoja membretada y dirigida al jefe de carrera, debe incluir nombre del alumno, nombre de proyecto, asesor por parte de la empresa, fecha de inicio y fecha final de residencias). Entregarlo a la encargada de residencias al final de su residencia.<br>
+
+    <p align="center">
+      <a target="_blank" data-toogle="tooltip"  title="Carta de terminacion  de residencias " download href='{{asset('documents/content/vinculacion/residencias/carta_terminacion_ver2.docx.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
+    <br>
 
 
-      <p align="center">
-        <a href='vinculacion/ad19/solicitud_residencias_2020.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar Solicitud de residencias' border='0'></a> </p>
-      <p align="center">Clic en la imagen para descargar documento</p>
+
+      6.- <strong>Carta de presentación.</strong>  <hr class="red">(La emite vinculación y se debe de entregar una copia a la encargada de residencias firmada y sellada por la empresa). Entregarlo a la encargada de residencias al inicio de su residencia y una copia a vinculación.<br>
       <br>
-
-      2.- <strong>Anteproyecto.</strong> firmado de aceptación por el revisor. Entregarlo a la encargada de residencias al inicio de su residencia.<br><br>
-
-      3.- <strong>Convenio de residencias profesinales.</strong> (Llenar el convenio y entregarlo a la encargada de residencias y una copia a vinculación).<br>
-
-      <p align="center">
-        <a href='vinculacion//fj19/cnvdw.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar Convenio de residencias profesinales' border='0'></a> </p>
-      <p align="center">Clic en la imagen para descargar documento</p>
+      7.- <strong>Constancia de Acreditación de servicio social.</strong>  <hr class="red">Entregarlo a la encargada de residencias al inicio de su residencia.<br>
       <br>
-      <br>
-      4.- <strong>Cardex con calificaciones.</strong> (tener aprobado al menos 80% de créditos de su plan de estudio). Entregarlo a la encargada de residencias al inicio de su residencia.<br>
-      <br>
-
-
-
-      5.- <strong>Carta de aceptación.</strong> (Hoja membretada y dirigida al jefe de carrera, debe incluir nombre del alumno, nombre de proyecto, asesor por parte de la empresa, fecha de inicio y fecha final de residencias). Entregarlo a la encargada de residencias al inicio de su residencia.<br>
-
-      <p align="center">
-        <a href='vinculacion/carta_aceptacionv1.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar Formato de evalución' border='0'></a> </p>  <p align="center">Clic en la imagen para descargar documento</p>
-
-
-
-      5.1.- <strong>Carta de terminación.</strong> (Hoja membretada y dirigida al jefe de carrera, debe incluir nombre del alumno, nombre de proyecto, asesor por parte de la empresa, fecha de inicio y fecha final de residencias). Entregarlo a la encargada de residencias al final de su residencia.<br>
-
-      <p align="center">
-        <a href='vinculacion/carta_terminacion_ver2.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar Formato de evalución' border='0'></a> </p>  <p align="center">Clic en la imagen para descargar documento</p>
-
-      <br>
-      6.- <strong>Carta de presentación.</strong> (La emite vinculación y se debe de entregar una copia a la encargada de residencias firmada y sellada por la empresa). Entregarlo a la encargada de residencias al inicio de su residencia y una copia a vinculación.<br>
-      <br>
-      7.- <strong>Constancia de Acreditación de servicio social.</strong> Entregarlo a la encargada de residencias al inicio de su residencia.<br>
-      <br>
-      8.- <strong>Acreditación de actividades complementarias.</strong> (La emite el jefe de carrera) Entregarlo a la encargada de residencias al inicio de sus residencias.
+      8.- <strong>Acreditación de actividades complementarias.</strong>  <hr class="red">(La emite el jefe de carrera) Entregarlo a la encargada de residencias al inicio de sus residencias.
       </p>
 
 
       <p>
       <h3><u>DOCUMENTOS A ENTREGAR DURANTE LAS REVISIONES DE RESIDENCIAS PROFESIONALES:</u></h3>
-      </p>
+      </p> <hr class="red">
 
-      <p>9.- <strong>Formatos de asesoría</strong>  V.- anexo V formato de registro de asesoría (esta la llenan junto con su asesor interno en cada revisión de residencias entregar a la encargada de residencias profesionales un formato por cada revisión de residencias, 4 en total).</p>
+      <p>9.- <strong>Formatos de asesoría</strong>  <hr class="red"> V.- anexo V formato de registro de asesoría (esta la llenan junto con su asesor interno en cada revisión de residencias entregar a la encargada de residencias profesionales un formato por cada revisión de residencias, 4 en total).</p>
 
-      <p align="center">
-        <a href='vinculacion/asesoriaad20.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar formato de asesoria' border='0'></a> </p>  <p align="center">Clic en la imagen para descargar documento</p>
-      <br>
+
+    <p align="center">
+      <a target="_blank" data-toogle="tooltip"  title="Formatos de asesorias  de residencias " download href='{{asset('documents/content/vinculacion/residencias/asesoriaad20.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
 
       <hr>
 
       <p><h3><u>DOCUMENTOS A ENTREGAR AL FINALIZAR LAS RESIDENCIAS PROFESIONALES:</u></h3></p>
-
-      <p>10.- <strong>Formato de Seguimiento de residencia.</strong> (CRONOGRAMA) Este formato se va llenando de acuerdo a las actividades q planearon y que van realizando y se sugiere que el asesor interno sea quien la guarde para que cuando vayan acudiendo a revisión la vayan llenando junto con su asesor interno). Entregarlo a la encargada de residencias al final de su residencia.<br>
-
-      <p align="center">
-        <a href='vinculacion/seguimientoad20.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar formato de seguimiento de residencia' border='0'></a> </p>  <p align="center">Clic en la imagen para descargar documento</p>
-      <br>
+    <hr class="red">
+      <p>10.- <strong>Formato de Seguimiento de residencia.</strong>  <hr class="red">(CRONOGRAMA) Este formato se va llenando de acuerdo a las actividades q planearon y que van realizando y se sugiere que el asesor interno sea quien la guarde para que cuando vayan acudiendo a revisión la vayan llenando junto con su asesor interno). Entregarlo a la encargada de residencias al final de su residencia.<br>
 
 
-      11.- <strong>Formato de evaluación XXIX Y XXX </strong>(debe tener sello y firma de la empresa), Entregarlo a la encargada de residencias al final de su residencia.) NOTA: El Anexo  XXIX se entrega en la segunda y cuarta revision y el formato XXX se entrega en la cuarta revision. En caso de que la empresa no cuenta con sello debe hacer un oficio en hoja membretada donde hace constar que no tiene sello y el alumno lo  entrega a la encargada de residencias profesionales. <br>
+    <p align="center">
+    <a target="_blank" data-toogle="tooltip"  title="Seguimiento de residencias " download href='{{asset('documents/content/vinculacion/residencias/seguimientoad20.docx')}}' type="button" download  class="btn btn-primary">     <i class='fas fa-download' style='font-size:20px'></i></a> </p>
+    <br>
+
+      11.- <strong>Formato de evaluación XXIX Y XXX </strong> <hr class="red">(debe tener sello y firma de la empresa), Entregarlo a la encargada de residencias al final de su residencia.) NOTA: El Anexo  XXIX se entrega en la segunda y cuarta revision y el formato XXX se entrega en la cuarta revision. En caso de que la empresa no cuenta con sello debe hacer un oficio en hoja membretada donde hace constar que no tiene sello y el alumno lo  entrega a la encargada de residencias profesionales. <br>
+
+    rvxxix
 
 
       <p align="center">
@@ -143,14 +149,14 @@
 
 
 
-      12.- <strong>Autorización de proyecto.</strong> <br>
+      12.- <strong>Autorización de proyecto.</strong>  <hr class="red">
 
       <p align="center">
         <a href='vinculacion/carta_autad20.docx'><img src='vinculacion/doc1.png' width='100' height='100' title='Descargar Formato de evalución' border='0'></a> </p>  <p align="center">Clic en la imagen para descargar documento</p>
 
       <br>
 
-      13.- <strong>Entregar disco con su proyecto final. </strong>(Se sugiere sea en pdf y debe de contener dentro del mismo documento, después de la portada la carta de terminación que expide la empresa y la carta de terminación que expide el asesor interno) Entregarlo a la encargada de residencias al final de su residencia.
+      13.- <strong>Entregar disco con su proyecto final. </strong> <hr class="red">(Se sugiere sea en pdf y debe de contener dentro del mismo documento, después de la portada la carta de terminación que expide la empresa y la carta de terminación que expide el asesor interno) Entregarlo a la encargada de residencias al final de su residencia.
       <br>
       <br>
 
@@ -160,7 +166,7 @@
 
       <p>
 
-        14.-<strong> Encuesta del asesor externo. </strong>Entregarlo a la encargada de residencias al final de su residencia.
+        14.-<strong> Encuesta del asesor externo. </strong> <hr class="red">Entregarlo a la encargada de residencias al final de su residencia.
         <br><br>
 
 
@@ -169,7 +175,7 @@
       <br>
 
 
-      15.- Encuesta del alumno. Al finalizar residencias, el alumno realiza encuesta de residencias profesionales  en la página del ITSCH, toma nota del número de folio que arroja al final y le pasa el número de folio a la encargada de residencias profesionales.<br>
+      15.- Encuesta del alumno.  <hr class="red">Al finalizar residencias, el alumno realiza encuesta de residencias profesionales  en la página del ITSCH, toma nota del número de folio que arroja al final y le pasa el número de folio a la encargada de residencias profesionales.<br>
 
       </p>
 
@@ -179,7 +185,7 @@
 
 
       <h3>Condiciones generales del convenio de Residencias Profesionales</h3>
-
+    <hr class="red">
 
 
       <p align="center">
@@ -195,7 +201,7 @@
       <h3>Lineamientos para la operación y acreditación de la residencia profesional</h3>
 
 
-
+    <hr class="red">
       <p align="center">
         <a href='vinculacion/lineamiento_para_la_operacion_y_acreditacion_de_la_residencia_profesional.pdf'><img src='vinculacion/pdf-icon.png' width='100' height='100' title='Descargar Lineamientos para la operacion y acreditación de la residencia profesional' border='0'></a></p>   <p align="center">Clic en la imagen para descargar documento</p>
       <br>
@@ -208,7 +214,7 @@
 
       <h3>Marco legal</h3>
 
-
+    <hr class="red">
 
       <p align="center">
         <a href='vinculacion/marco_legal_v8.pdf'><img src='vinculacion/pdf-icon.png' width='100' height='100' title='Descargar marco legal' border='0'></a></p>   <p align="center">Clic en la imagen para descargar documento</p>
@@ -225,11 +231,10 @@
 
 
 
-      <hr>
-
+    <hr class="red">
 
       <h3>Directorio de empresas en Michoacán</h3>
-
+    <hr class="red">
       <p align="center">
         <a href='vinculacion/dir_emp_michoacan_res.pdf'><img src='vinculacion/pdf-icon.png' width='100' height='100' title='Descargar directorio de empresas en Queretaro' border='0'></a></p>   <p align="center">Clic en la imagen para descargar documento</p>
       <br>
@@ -238,11 +243,11 @@
 
 
 
-      <hr>
+    <hr class="red">
 
 
       <h3>Directorio de empresas en Celaya</h3>
-
+    <hr class="red">
       <p align="center">
         <a href='vinculacion/dir_emp_celaya_res.pdf'><img src='vinculacion/pdf-icon.png' width='100' height='100' title='Descargar directorio de empresas en Queretaro' border='0'></a></p>   <p align="center">Clic en la imagen para descargar documento</p>
       <br>
@@ -251,7 +256,7 @@
 
 
 
-      <hr>
+    <hr class="red">
 
 
 
@@ -259,7 +264,7 @@
 
 
       <h3>Directorio de empresas en Querétaro</h3>
-
+    <hr class="red">
       <p align="center">
         <a href='vinculacion/dir_emp_queretaro_res.pdf'><img src='vinculacion/pdf-icon.png' width='100' height='100' title='Descargar directorio de empresas en Queretaro' border='0'></a></p>   <p align="center">Clic en la imagen para descargar documento</p>
       <br>
@@ -268,16 +273,16 @@
 
 
 
-      <hr>
+    <hr class="red">
       <h3>Directorio de empresas en Toluca</h3>
-
+    <hr class="red">
 
       <p align="center">
         <a href='vinculacion/dir_emp_toluca_res.pdf'><img src='vinculacion/pdf-icon.png' width='100' height='100' title='Descargar directorio de empresas en Toluca' border='0'></a></p>
 
 
       <p align="center">Clic en la imagen para descargar documento</p>
-      <br>
+    <hr class="red">
 
     </article>
   
