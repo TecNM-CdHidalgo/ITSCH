a5a23de71e68a5752521ba549a9012cc750bca9e
Oscar Delgado <30932646+kioselsa@users.noreply.github.com>
2020-09-17 23:21:53 -0500

    Ajustesn en pagina principal y menus
    Se ajusto la imagen de ubicacion para que sea directa de maps, ademas se cambio el tamaño de los menus por un porcentaje vh, tomandolo del tamaño del medio de visualizacion

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -183,9 +183,7 @@
 	                </p>
 	            </div>
 	            <div style="width: 100%" style="text-align: center;">
-	                <a href='#' title='Ver Ubicación'>
-	                    <img src="http://www.cdhidalgo.tecnm.mx/images/footer/Foto_Ubi.JPG" alt="Ubicación" style='width:95%;' />
-	                </a>
+	               <iframe src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d3213.225482098154!2d-100.51845922539054!3d19.708400592036977!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e1!3m2!1ses-419!2smx!4v1600400116125!5m2!1ses-419!2smx" width="45%" height="280" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
 	            </div>
 	        </div>
 	        <div>                
