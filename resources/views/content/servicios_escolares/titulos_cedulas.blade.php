b44d318cdf65bca2b2ad1e76804f6367a4452d2a
Oscar Delgado <30932646+kioselsa@users.noreply.github.com>
2020-09-18 12:08:38 -0500

    Vista de titulacion
    Se genero la vista de titulacion con todos sus archivos

--- resources/views/content/servicios_escolares/titulos_cedulas.blade.php
+++ resources/views/content/servicios_escolares/titulos_cedulas.blade.php
@@ -2,145 +2,160 @@
 
 @section('content')
 
-
-
     <!-- Article main content -->
-    <article class="col-md-8 maincontent">
-        <header class="page-header">
-            <h1 class="page-title">Titulación y Cédulas:</h1>
-        </header>
+   <div class="row">
+       <div class="col-sm-1"></div>
+       <div class="col-sm-10">
+            <header class="page-header" style="text-align: center;">
+                <h1 class="page-title">Titulación y Cédulas:</h1>
+            </header>
+            <hr class="red">
+
+            <!-- contenido bruto dinamico-->
+            <span class="media thumbnail"><img src="{{asset('images/content/servicios escolares/titulacion.png')}}"></span>
 
 
-        <!-- contenido bruto dinamico-->
+            <br><br>
+            Cualquier duda o aclaración de forma individual les atenderemos en servicios escolares o vía telefónica al 786 154 9000 ext. 125 o 167, también pueden enviarnos sus dudas y seguimiento de trámites a través del siguiente link:
+            <a href=”https://web.facebook.com/Becas-de-Manutenci%C3%B3n-ITSCH-883383695106559/”>https://web.facebook.com/Becas-de-Manutenci%C3%B3n-ITSCH-883383695106559/</a>
+
+            <br><br>
+           
+            <hr class="red">
+           
+
+            <p>Los procedimientos y requisitos están ya disponibles, para los alumnos interesados en realizar su titulación, estos documentos siempre estaran vigentes,
+                cualquier cambio se les notificará en la página principal, los documentos y procedimientos los pueden
+                descargar los siguientes enlaces:</p>
+            <br><br>
+
+            <div class="row">   
+                <div class="col-sm-1"></div>
+                <div class="col-sm-2">
+                    <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/procedimiento_tit_v12.pdf ')}}" data-toogle="tooltip" title="Requisitos y Procedimiento de Titulación." download  target="_blank">
+                        <i class='fas fa-download' style='font-size:20px'></i>
+                    </a>
+                </div>
+                <div class="col-sm-2">
+                    <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/for_no_adeudo_tit_v3.docx ')}}" data-toogle="tooltip" title="Constancia de no Adeudo para alumnos próximos a Titularse." download  target="_blank">
+                        <i class='fas fa-download' style='font-size:20px'></i>
+                    </a>
+                </div>
+                <div class="col-sm-2">
+                   <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/tit_tesv0.pdf ')}}" data-toogle="tooltip" title="Titulación por tesis." download  target="_blank">
+                        <i class='fas fa-download' style='font-size:20px'></i>
+                    </a>
+                </div>
+                <div class="col-sm-2">
+                    <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/tit_tesv0.pdf ')}}" data-toogle="tooltip" title="Proyecto de investigación." download  target="_blank">
+                        <i class='fas fa-download' style='font-size:20px'></i>
+                    </a>
+                </div>
+                <div class="col-sm-2">
+                    <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/tit_inf_tec_resv0.pdf ')}}" data-toogle="tooltip" title="Titulación por informe Técnico de Residencias profesionales." download  target="_blank">
+                        <i class='fas fa-download' style='font-size:20px'></i>
+                    </a>
+                </div>
+                <div class="col-sm-1"></div>
+            </div> 
+            <br>            
+
+            <hr class="red">
+
+            <p> En esta sección podras encontraras todos los documentos y formatos necesarios para tramitar tu titulaciòn</p>
+
+            <p>                
+                1.- <strong>Formato de registro de proyecto.</strong>
+            </p>
+
+            <br>
+
+            <p align="center">
+                <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/registro_titulacionv14.docx ')}}" data-toogle="tooltip" title="Formato de registro de proyecto." download  target="_blank">
+                    <i class='fas fa-download' style='font-size:20px'></i>
+                </a>
+            </p>
+            <br>
+
+            <hr class="red">
+
+            <p>                
+                2.- <strong>Solicitud del estudiante.</strong>
+            </p>
+
+            <br>
 
 
-        <span class="media thumbnail"><img src="../media/servicios_escolares/titulacion/titulacion.png"></span>
+            <p align="center">
+
+                <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/solicitud_titulacionv14.docx ')}}" data-toogle="tooltip" title="Formato de solicitud de titulación." download  target="_blank">
+                    <i class='fas fa-download' style='font-size:20px'></i>
+                </a>
+               
+            </p>           
+            <br>
+
+            <hr class="red">
+
+            <p>                
+                3.- <strong>Formato de liberación del proyecto para la Titulación integral.</strong>
+            </p>
+
+            <br>
 
 
-        <br><br>
-        Cualquier duda o aclaración de forma individual les atenderemos en servicios escolares o vía telefónica al 786 154 9000 ext. 125 o 167, también pueden enviarnos sus dudas y seguimiento de trámites a través del siguiente link:
-        <a href=”https://web.facebook.com/Becas-de-Manutenci%C3%B3n-ITSCH-883383695106559/”>https://web.facebook.com/Becas-de-Manutenci%C3%B3n-ITSCH-883383695106559/</a>
+            <p align="center">
+                 <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/liberacion_proyectov14.docx ')}}" data-toogle="tooltip" title="Formato de liberación." download  target="_blank">
+                    <i class='fas fa-download' style='font-size:20px'></i>
+                </a>
+            </p>
+            <br>
 
-        <br><br>
-        <!--
-    ATENTAMENTE
-    SERVICIOS ESCOLARES
-     </p>
-
-
-     <span class="media thumbnail"><img src="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/fj19/1.png"></span>
-    -->
-        <hr>
-        <br><br>
-
-        <p>Los procedimientos y requisitos están ya disponibles, para los alumnos interesados en realizar su titulación, estos documentos siempre estaran vigentes,
-            cualquier cambio se les notificará en la página principal, los documentos y procedimientos los pueden
-            descargar los siguientes enlaces:</p>
-        <br><br>
-
-        <p align="center"><a href="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/fj19/procedimiento_tit_v12.pdf"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Requisitos y Procedimiento de Titulación.</strong></a></p>
-
-        <p align="center"><a href="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/fj19/for_no_adeudo_tit_v3.docx"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Constancia de no Adeudo para alumnos próximos a Titularse.</strong></a></p>
-
-        <p align="center"><a href="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/tit_tesv0.pdf"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Titulación por tesis.</strong></a></p>
-
-        <p align="center"><a href="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/tit_tesv0.pdf"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Proyecto de investigación.</strong></a></p>
+            <hr class="red">
 
 
-        <p align="center"><a href="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/tit_inf_tec_resv0.pdf"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Titulación por informe Técnico de Residencias profesionales.</strong></a></p>
+            <p>                
+                4.- <strong>Portada para discos de Titulación.</strong>
+            </p>
 
-        <br><br>
-
-        <hr>
-
-        <p> En esta sección podras encontraras todos los documentos y formatos necesarios para tramitar tu titulaciòn</p>
-
-        <p>                1.- <strong>Formato de registro de proyecto.</strong>
-        </p>
-
-        <br>
+            <br>
 
 
-        <p align="center">
-            <a href='../media/servicios_escolares/titulacion/fj19/registro_titulacionv14.docx'><img src='../media/servicios_escolares/titulacion/doc2.png' width='100' height='100' title='Descargar Formato de registro de proyecto' border='0'></a> </p>
-        <p align="center"> Clic en la imagen para descargar documento </p>
-        <br>
+            <p align="center">
+                 <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/portada_discos_titulacionv1.docx ')}}" data-toogle="tooltip" title="Portadas de discos." download  target="_blank">
+                    <i class='fas fa-download' style='font-size:20px'></i>
+                </a>
+            </p>
+            <br>
 
-        <hr>
+            <hr class="red">
 
+            <p>                
+                5.- <strong>Portada para empastado de Titulación.</strong>
+            </p>
 
-
-        <p>                2.- <strong>Solicitud del estudiante.</strong>
-        </p>
-
-        <br>
-
-
-        <p align="center">
-            <a href='../media/servicios_escolares/titulacion/fj19/solicitud_titulacionv14.docx'><img src='../media/servicios_escolares/titulacion/doc2.png' width='100' height='100' title='Descargar solicitud del estudiante' border='0'></a> </p>
+            <br>
 
 
-        <p align="center">Clic en la imagen para descargar documento</p>
-        <br>
+            <p align="center">
+                <a type="button" class="btn btn-primary" href="{{asset('documents/content/servicios escolares/portada_empastado.doc ')}}" data-toogle="tooltip" title="Portadas del empastado." download  target="_blank">
+                    <i class='fas fa-download' style='font-size:20px'></i>
+                </a>
+            </p>
+            <br>
 
-        <hr>
+            <hr class="red">
+            <p align="center">
+                 <img src="{{asset('images/content/servicios escolares/titulate.png')}}" width="50%" height="300">
+            </p>
+           
+            <hr class="red">
 
-        <p>                3.- <strong>Formato de liberación del proyecto para la Titulación integral.</strong>
-        </p>
-
-        <br>
-
-
-        <p align="center">
-            <a href='../media/servicios_escolares/titulacion/fj19/liberacion_proyectov14.docx'><img src='../media/servicios_escolares/titulacion/doc2.png' width='100' height='100' title='Descargar formato de liberacion' border='0'></a> </p>
-        <p align="center">Clic en la imagen para descargar documento</p>
-        <br>
-
-        <hr>
-
-
-        <p>                4.- <strong>Portada para discos de Titulación.</strong>
-        </p>
-
-        <br>
-
-
-        <p align="center">
-            <a href='../media/servicios_escolares/titulacion/portada_discos_titulacionv1.docx'><img src='../media/servicios_escolares/titulacion/doc2.png' width='100' height='100' title='Descargar portada para discos de titulación' border='0'></a> </p>
-        <p align="center">Clic en la imagen para descargar documento</p>
-        <br>
-
-        <hr>
-
-
-
-
-
-        <p>                5.- <strong>Portada para empastado de Titulación.</strong>
-        </p>
-
-        <br>
-
-
-        <p align="center">
-            <a href='../media/servicios_escolares/titulacion/portada_empastado.doc'><img src='../media/servicios_escolares/titulacion/doc2.png' width='100' height='100' title='Descargar de empastado de titulación' border='0'></a> </p>
-        <p align="center">Clic en la imagen para descargar documento</p>
-        <br>
-
-        <hr>
-
-        <hr>
-
-        <img src="../media/servicios_escolares/titulacion/titulate.png">
-        <br>
-        <p align="center"><a href="../media/tecnologico/formatos-descarga/servicios-escolares/titulacion/motiv1.pptx"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Descarga la presentación.</strong></a></p>
-
-        <blockquote>  </blockquote>
-
-    </article>
-    <!-- /Article -->
-
-
-
-
+            <p align="center">
+                <a download href="{{asset('documents/content/servicios escolares/motiv1.pptx')}}"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Descarga la presentación.</strong>
+                </a>
+            </p>
+       </div>
+       <div class="col-sm-1"></div>
+   </div>
 @endsection
