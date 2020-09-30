2ac0c5d96ac1074f274374c19bb8e547207638ae
hackghost <higueragiovani@gmail.com>
2020-09-30 10:34:09 -0700

    Seccion servicio social

--- resources/views/content/vinculacion/servicio-social.blade.php
+++ resources/views/content/vinculacion/servicio-social.blade.php
@@ -6,5 +6,180 @@
     <h3>Servicio social</h3>
 
 
+			<tr>
+					<td class="posgrados" width="100%">
+						<h3>Servicio Social:</h3>
+						<p>
+						<!-- contenido bruto dinamico-->
+						<span class="media thumbnail" style="margin-left: 165px"><img src="./media/vinculacion/servicio-social/slide.jpg"></span>
+					</p>
+					</td>
+				</tr>
+
+				<tr>
+					<td>
+
+						<p align="justify">
+							Si ya cubriste el 70% de créditos del plan de estudios, ya estás en condiciones de realizar
+							tu Servicio Social.
+						</p>
+						<p align="center" style="font-size:20px;"><strong>Pero… ¿Qué es el servicio social?</strong></p>
+						<p>El Servicio Social es una <strong>prestación de carácter obligatorio por Ley y voluntario por
+								vocación con disposición personal</strong>. Es la aportación a la sociedad de los
+							conocimientos adquiridos en el aula, además de que permite la confrontación de la teoría y
+							la práctica.</p>
+						<p>El servicio social está contemplado en la retícula de los planes de estudio de las carreras
+							que se imparten en el Instituto Tecnológico Superior de Ciudad Hidalgo y es un requisito
+							para la titulación.</p>
+						<hr>
+						<h5><strong>Fechas para darse de alta en el Servicio Social:</strong></h5>
+						<p>Únicamente podrás darte de alta en los meses de febrero y agosto de cada año.</p>
+						<hr>
+
+						<h5><strong>Duración del Servicio Social:</strong></h5>
+						<p>El plazo para dar el servicio social es de 6 meses mínimo y 1 años como máximo, cubriendo un
+							total de 480 horas.</p>
+						<hr>
+						<h5><strong>Horario de atención:</strong></h5>
+						<p>El área de Servicio Social les atenderá en el horario de 9:00 a 12:00 hrs. y de 12:30 a 17:00
+							hrs.</p>
+						<hr>
+						<span class="cuadro-enfasis">
+							Para requisitos, recuerda cumplir con la entrega a tiempo de tus documentos.
+						</span>
+						<hr>
+
+						<p align="justify"> A continuación publicamos el formato unico de solicitud de registro que se
+							debera de entregar el <u><strong>dia Miercoles 8 de febrero del 2017 hasta las 16:00
+									hrs.</strong></u></p>
+
+						<p align="justify"> <strong> Los alumnos que no cumplan con estos requisitos en tiempo y forma
+								no podran liberar su servicio social. </strong></p>
+						<br>
+						<h3> Registro Automático </h3>
+
+						<h4> <a href="./apps/ServicioSocial/ServicioSocialAlumnos/index.html"> Clic aquí para ir al
+								sistema y capturar tus datos.</a>
+						</h4>
+
+						<br>
+						<br>
+						<br>
+						<br>
+
+
+						<!-- 
+
+						<h3> Solicitud de registro </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/registro.pdf"
+								target="_blank"><img src="./media/descargas/descarga.gif"></a></p>
+
+
+						<h3> Carta de liberacion dirigida a la CESS </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/Carta_lib_CESS.doc"
+								target="_blank"><img src="./media/descargas/descarga_word.gif"></a></p>
+
+
+						<h3> Reporte Bimestral </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/ad_18/reporte_bimestralv11.docx"
+								target="_blank"><img src="./media/descargas/descarga_word.gif"></a></p>
+
+
+						<h3> Evaluación Unidad Receptora </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/ad_18/eval_uni_recep.pdf"
+								target="_blank"><img src="./media/descargas/descarga.gif"></a></p>
+
+
+						<h3> Informe Global </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/ad_18/inf_global.pdf"
+								target="_blank"><img src="./media/descargas/descarga.gif"></a></p>
+
+
+						<h3> Informe Bimestral </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/ad_18/info_bim.pdf"
+								target="_blank"><img src="./media/descargas/descarga.gif"></a></p>
+
+						<h3> Evalución Bimestral </h3>
+
+						<p align="center"><a
+								href="./media/tecnologico/formatos-descarga/vinculacion/servicio-social/ad_18/evaluacion_bimestral.doc"
+								target="_blank"><img src="./media/descargas/descarga_word.gif"></a></p>
+
+						<br>
+
+
+						<!-- 
+                      
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/00Guia_para_registro_de_programa_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>00 Guia general para registro de programa</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/01 Solicitud de SS.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>01 Solicitud de Servicio Social</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/02 Carta compromiso.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>02 Carta compromiso</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/SolisRegiv1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>03 Solicitud de registro</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/04Carta_de_aceptacion_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>04 Carta de aceptación</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/05Informe_bimestral_1_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>05 Informe Bimestral 1</strong></a></p>
+              
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/06 Formato de evaluacion bimestral.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>06 Formato de evaluación bimestral</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/07Informe_bimestral_2_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>07 Informe Bimestral 2</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/08Informe_bimestral_3_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>08 Informe Bimestral 3</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/09_Informe_Global_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>09 Informe Global</strong></a></p>
+              
+              
+              <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/10_Evaluacion_de_la_unidad_receptora_v1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>10 Evaluación de la unidad receptora</strong></a></p>
+              
+              
+               <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/11Carta_de_terminacion_dirigida_al_ITSCHv2.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>11 Carta de terminación dirigida al ITSCH</strong></a></p>
+               
+               
+                <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/12 Carta de liberacion dirigida a la CESSv1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>12 Carta de liberación dirigida a la CESS</strong></a></p>
+                
+                
+                 <p><a href="../media/tecnologico/formatos-descarga/vinculacion/servicio-social/solicitud de registro de programav1.doc"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>13 Solicitud para la guía de registro de programa</strong></a></p>
+                
+                
+                
+                
+               <p>Esta información tambien esta disponible desde <a class="contenido" href="../tecnologico/formatos-descarga"> Documentos para Descargar</a> en el apartado de Servicios Social y lo puedes buscar por su nombre de cada documento.</p><hr>
+               
+               /Article -->
+
+
+						<blockquote>
+							A T E N T A M E N T E
+							<br> Oficina de Servicio Social
+						</blockquote>
+
+					</td>
+				</tr>
+
+			</article>
 
 @endsection
