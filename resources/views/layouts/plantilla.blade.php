c00cc9ba7eea4a35105e2a41d8644171450fa8ab
hackghost <higueragiovani@gmail.com>
2020-09-30 16:31:25 -0700

    seccion encuesta para seguimiento a egresados

--- resources/views/layouts/plantilla.blade.php
+++ resources/views/layouts/plantilla.blade.php
@@ -140,7 +140,7 @@
           Egresados/Empleadores
         </a>
         <div class="dropdown-menu">
-            <a class="dropdown-item" href="">Seguimiento a egresados</a>
+            <a class="dropdown-item" href="https://docs.google.com/forms/d/e/1FAIpQLSf6vB2zpwjLKVX4GdQnrpHLiLQWzcKo3u6J0v_DOYEgryZMig/viewform">Seguimiento a egresados</a>
             <a class="dropdown-item" href="">Encuesta de Empleadores</a>
         </div>
       </li>
