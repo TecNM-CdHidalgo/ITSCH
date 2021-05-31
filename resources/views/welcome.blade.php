b926809b99f4fe3d7df715fa0b1e01784d56154b
Oscar Delgado <30932646+kioselsa@users.noreply.github.com>
2021-05-31 12:10:16 -0500

    Actualización OCC

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -63,7 +63,7 @@
 		<div class="col-sm-3"></div>
 		<div class="col-sm-5"></div>
 		<div class="col-sm-1">
-			<div id=""></div>
+			<div id="occ-widget"></div>
 		</div>
 	</div>
     <hr class="red">
