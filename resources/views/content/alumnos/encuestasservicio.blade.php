7cda9a9714f653bc196c980f0255fd29eb150c78
Oscar Delgado <30932646+kioselsa@users.noreply.github.com>
2021-11-29 09:33:30 -0600

    Ajustes de formulario google

--- resources/views/content/alumnos/encuestasservicio.blade.php
+++ resources/views/content/alumnos/encuestasservicio.blade.php
@@ -7,7 +7,7 @@
         <div class="col-sm-1"></div>
         <div class="col-sm-10">
             {{-- Liga del formulario de google --}}
-            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdxV-Ad9ZJLLdgxF269ekbaNKdLbbA20nsJmugZc9W-wd_I2A/viewform?embedded=true" width="1024" height="500" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
+            <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdxV-Ad9ZJLLdgxF269ekbaNKdLbbA20nsJmugZc9W-wd_I2A/viewform?embedded=true" width="100%" height="600px" frameborder="0" marginheight="0" marginwidth="0">Cargando…</iframe>
         </div>
         <div class="col-sm-1"></div>
     </div>
