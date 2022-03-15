5a48bb08bb5eb009302646cbdea32329799493d7
kioselsa <30932646+kioselsa@users.noreply.github.com>
2022-03-15 10:08:30 -0600

    Se añadió analytics
    Se añadió la etiqueta de Google Analitics, para recopilar la información de visitas de la pagina.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -76,6 +76,16 @@
                 }
             }
         </script>
+
+       <!-- Global site tag (gtag.js) - Google Analytics -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-K3N5TM4CFJ"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-K3N5TM4CFJ');
+        </script>
     </head>
 
 
