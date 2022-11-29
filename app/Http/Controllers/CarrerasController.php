807ffa7e67ea8414bb116bb295072792f62f4775
kioselsa <30932646+kioselsa@users.noreply.github.com>
2022-11-29 11:41:41 -0600

    Prueba especialidades 2

--- app/Http/Controllers/CarrerasController.php
+++ app/Http/Controllers/CarrerasController.php
@@ -1119,9 +1119,9 @@ class CarrerasController extends Controller
         $pro_esp=Especialidad::select('id_programa')->where('id',$id_esp)->get();
         $programa=Programa::where('id',$pro_esp[0]->id_programa)->get();
 
-        foreach($materias_esp as $me){
+       /* foreach($materias_esp as $me){
             $me->link = asset('storage/carreras_planes_estudio/'.$programa[0]->nombre.'/especialidad/'.$me->nom_archivo);
-        }
+        }*/
          return response()->json($materias_esp);
     }
 
