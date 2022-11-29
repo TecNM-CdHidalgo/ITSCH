2602c595e55c51c733bf392e012b80507c6818f8
kioselsa <30932646+kioselsa@users.noreply.github.com>
2022-11-29 11:05:08 -0600

    Cambios en Igualdad

--- resources/views/content/normativos/igualdad.blade.php
+++ resources/views/content/normativos/igualdad.blade.php
@@ -30,7 +30,7 @@
                     <img class="card-img-top" src="{{asset('images/content/normativos/igualdad/3.jpg')}}" alt="Card image" style="width:100%">
                     <div class="card-body">
                       <h4 class="card-title" style="text-align: center;">Equidad de Género</h4>
-                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;">La Secretaría del Trabajo y Previsión Social (STPS) a partir del 2009, instrumenta la Norma Mexicana NMX-R-025-SCFI-2009 para la Igualdad Laboral entre Mujeres y Hombres. El objetivo de esta Norma se estableció con el fin de evaluar y certificar las prácticas en materia de igualdad laboral y no discriminación. <br> <b>Leer mas...</b> <br><br><br></p>
+                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;">La Secretaría del Trabajo y Previsión Social (STPS) a partir del 2009, instrumenta la Norma Mexicana NMX-R-025-SCFI-2009 para la Igualdad Laboral entre Mujeres y Hombres. El objetivo de esta Norma se estableció con el fin de evaluar y certificar las prácticas en materia de igualdad laboral y no discriminación. <br> <b>Leer mas...</b> <br><br><br><br><br><br><br><br></p>
                       <a href="{{asset('documents/content/normativos/igualdad/POLITICA DE IGUALDAD LABORAL.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
                     </div>
                 </div>
@@ -40,7 +40,7 @@
                     <img class="card-img-top" src="{{asset('images/content/normativos/igualdad/4.jpg')}}" alt="Card image" style="width:100%">
                     <div class="card-body">
                       <h4 class="card-title" style="text-align: center;">Acoso laboral</h4>
-                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;">De  acuerdo  a  la  Norma  Mexicana en  igualdad  laboral y  no  discriminación,  EL  ACOSO  SEXUAL  ES: Una forma de violencia con connotación lasciva en la que, si bien no existe la subordinación, hay un ejercicio abusivo de poder que conlleva a un estado de indefensión y de riesgo para la victima, independientemente de queserealice en uno o varios eventos: <br> <b>Leer mas...</b></p>
+                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;">De  acuerdo  a  la  Norma  Mexicana en  igualdad  laboral y  no  discriminación,  EL  ACOSO  SEXUAL  ES: Una forma de violencia con connotación lasciva en la que, si bien no existe la subordinación, hay un ejercicio abusivo de poder que conlleva a un estado de indefensión y de riesgo para la victima, independientemente de queserealice en uno o varios eventos: <br> <b>Leer mas...</b> <br><br><br><br><br><br> </p>
                       <a href="{{asset('documents/content/normativos/igualdad/PREVENCION DE HOSTIGAMIENTO.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
                     </div>
                 </div>
@@ -49,9 +49,9 @@
                 <div class="card" style="width:300px">
                     <img class="card-img-top" src="{{asset('images/content/normativos/igualdad/1.jpg')}}" alt="Card image" style="width:100%">
                     <div class="card-body">
-                      <h4 class="card-title" style="text-align: center;">Códido de ética</h4>
-                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;">El código de ética será aplicable a todas las personas que desempeñen un empleo, cargo o comisión, al interior de alguna dependencia o entidad de la Administración Pública Federal, o bien, en alguna empresa productiva del Estado. <br> <b>Leer mas...</b> <br><br><br><br><br></p>
-                      <a href="{{asset('documents/content/normativos/igualdad/CODIGO DE ETICA Y DE CONDUCTA.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
+                      <h4 class="card-title" style="text-align: center;">Códido de conducta</h4>
+                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;"> La NMX-R-025-SCFl-2015 es la encargada de propiciar la integridad de los servidores públicos y para implementar acciones permanentes que favorezcan su comportamiento ético, dentro de la norma nos menciona el CODIGO DE CONDUCTA, lo cual permite que el personal que desempeña un empleo, cargo, comisión o función, deberá conducir su actuación pública con trasparencia, honestidad, lealtad, cooperación, austeridad, sin ostentación y con una clara orientación al interés público. <br> <b>Leer mas...</b> </p>
+                      <a href="{{asset('documents/content/normativos/igualdad/CODIGO DE CONDUCTA DEL TECNOLOGICO NACIONAL DE MEXICO.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
                     </div>
                 </div>
             </div>
@@ -97,12 +97,12 @@
                 <div class="card" style="width:300px">
                     <img class="card-img-top" src="{{asset('images/content/normativos/igualdad/8.jpg')}}" alt="Card image" style="width:100%">
                     <div class="card-body">
-                      <h4 class="card-title" style="text-align: center;">POLITICA DE IGUALDAD LABOARAL Y NO DISCRIMINACIÓN</h4>
+                      <h4 class="card-title" style="text-align: center;">Politica de igualdad laboral y no discriminación</h4>
                       <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;">El Tecnológico Nacional de México a través de su Director General, manifiesta su compromiso con
                         la defensa de los derechos humanos, por lo que en la esfera de su competencia garantizará el
                         principio de igualdad sustantiva entre mujeres y hombres en el ejercicio de sus derechos laborales,
                         así como el derecho fundamental a la no discriminación en los procesos de ingreso, formación y
-                        promoción profesional...<br> <b>Leer mas...</b> <br></p>
+                        promoción profesional...<br><br><br> <b>Leer mas...</b> <br></p>
                       <a href="{{asset('documents/content/normativos/igualdad/poli.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
                     </div>
                 </div>
@@ -119,6 +119,32 @@
             </div>
 
             <div class="col-sm-4">
+                <div class="card" style="width:300px">
+                    <img class="card-img-top" src="{{asset('images/content/normativos/igualdad/2.jpg')}}" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                      <h4 class="card-title" style="text-align: center;">Lactancia materna</h4>
+                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;"> <b>La OMS Y UNICEF</b>  <br> Mencionan que La lactancia materna les proporciona a los bebés todos los nutrientes que necesitan para crecer y que su sistema inmunológico se desarrolle plenamente. Recomiendan que la lecha materna sea el alimento exclusivo de los bebés recién nacidos hasta los 6 meses de edad, y que hasta los 2 años se alimenten con una combinación de la misma con alimentos adecuados y nutritivos para su edad <br><br><br><br> <b>Leer mas...</b></p>
+                      <a href="{{asset('documents/content/normativos/igualdad/LACTANCIA MATERNA.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <br>
+        <div class="row">
+            <div class="col-sm-4">
+                <div class="card" style="width:300px">
+                    <img class="card-img-top" src="{{asset('images/content/normativos/igualdad/2.jpg')}}" alt="Card image" style="width:100%">
+                    <div class="card-body">
+                      <h4 class="card-title" style="text-align: center;">Código de Ética de la Administración Publica Federal</h4>
+                      <p class="card-text" style="text-align: justify; font-family: 'Montserrat'; font-size: 14px;"> El presente Código de Ética es aplicable a todas las personas que desempeñen un empleo, cargo o comisión en las dependencias y entidades de la Administración Pública Federal, por lo que es obligatorio en cualquiera de sus niveles jerárquicos, incluyendo al personal de base y sindicalizado, y su incumplimiento será objeto de denuncia, conforme a lo dispuesto en el artículo 24 del presente instrumento. <br><br><br><br> <b>Leer mas...</b></p>
+                      <a href="{{asset('documents/content/normativos/igualdad/Código de Ética de la Administración Publica Federal.pdf')}}" class="btn btn-primary" target="_blank">Ver/descargar</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-sm-4">
+
+            </div>
+            <div class="col-sm-4">
 
             </div>
         </div>
@@ -126,7 +152,12 @@
         <hr>
         <p style="text-align: justify;  font-family: 'Montserrat'; font-size: 20px;">Si has sufrido algún tipo de acoso y/o discriminación denuncia aquí...</p>
         <a href="{{ route('contenido.denuncia.index') }}" type="button" class="btn btn-primary">Denuncia</a>
+        <hr>
+        <h4>Certificado NMX-R-025-SCFI-2015</h4>
         <br>
-        <br>
+        <img style="max-height: 500px" src="{{ asset('documents/content/normativos/igualdad/CERTIFICADO-2025_162 SUP. DE CD. HIDALGO_001.jpg') }}" class="float-left img-fluid img-thumbnail" alt="Imagen de certificado 1">
+        <img style="max-height: 500px" src="{{ asset('documents/content/normativos/igualdad/CERTIFICADO-2025_162 SUP. DE CD. HIDALGO_002.jpg') }}" class="float-right img-fluid img-thumbnail" alt="Imagen de certificado 2">
+
+        <div style="margin-top: 600px"></div>
 
 @endsection
