b37d9b995a4f843ce70f87d4c9176e123b411060
kioselsa <30932646+kioselsa@users.noreply.github.com>
2023-08-03 08:24:09 -0600

    Prueba

---
+++ resources/views/content/alumnos/exani2.blade.php
@@ -0,0 +1,323 @@
+@extends('layouts.app')
+
+
+@section('content')
+<div class="row">
+    <div class="col-sm-1"></div>
+    <div class="col-sm-10">
+        <h3>Examenes de nuevo ingreso</h3>
+        <hr>
+
+        <!-- Ingeniería en sistemas -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/sistemas.png') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en sistemas computacionales" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería es sistemas computacionales </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería en sistemas computacionales el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a href="https://forms.gle/NCxBE4RvKpQrvWhGA" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en sistemas -->
+
+
+        <!-- Ingeniería industrial -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/industrial.png') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería industrial" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería industrial </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería industrial el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a href="https://forms.gle/HgeUfHsym5zPzv448" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería industrial -->
+
+
+        <!-- Ingeniería bioquimica -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/bioquimica.jpg') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería bioquímica" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería Bioquímica </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería bioquímica el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a href="https://forms.gle/zzuXdiFxCyT5Vhw68" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de bioquimica -->
+
+
+        <!-- Ingeniería en tics -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/tics.png') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en TICS" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en TICS </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería en TICS el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a href="https://forms.gle/PH3KBHR3ZAyAiAVp8" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en tics -->
+
+
+        <!-- Ingeniería en gestion -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/gestion.jpg') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en Gestión empresarial" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en gestión empresarial </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería gestión empresarial el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a href="https://forms.gle/xQkawb1iDSS8RZPV8" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en gestion -->
+
+
+        <!-- Ingeniería en mecatronica -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/meca.jpg') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en mecatrónica" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en mecatrónica </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería mecatrónica el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a href="https://forms.gle/V5UPDzqZ5mLZr2f49" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en mecatronica -->
+
+
+        <!-- Ingeniería en nano -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/nano.png') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en Nanotecnología" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en nanotecnología </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería en nanotecnología el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a target="_blank" href="https://forms.gle/wpKs4rW4PLpzzfHq7" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en nano -->
+
+    </div>
+    <div class="col-sm-1"></div>
+</div>
+
+
+@endsection
