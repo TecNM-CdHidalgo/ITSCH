fd88a911c43dc7dedb5408c52d4b553bdd04d1e0
kioselsa <30932646+kioselsa@users.noreply.github.com>
2023-08-11 17:05:11 -0600

    Corrección carrusel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -59,18 +59,18 @@
             @foreach($noticias as $not)
                 @if($not->resaltar==1)
                     @if($ban)
-                        <div class="carousel-item active text-center" id="contCarousel">
+                        <div class="carousel-item active text-center">
                             <a href="{{route('ver',$not->id)}}">
-                                <img src="{{ asset('storage/noticias/imagenes/'.$not->imagen) }}" alt="{{$not->titulo}}" id="imgCarr" >
+                                <img src="{{ asset('storage/noticias/imagenes/'.$not->imagen) }}" alt="{{$not->titulo}}"  class="d-block w-100 img-fluid rounded imgFormCarousel" >
                             </a>
                         </div>
                         @php
                             $ban=false;
                         @endphp
                     @else
-                        <div class="carousel-item text-center" id="contCarousel">
+                        <div class="carousel-item text-center">
                             <a href="{{route('ver',$not->id)}}">
-                                <img src="{{ asset('storage/noticias/imagenes/'.$not->imagen) }}" alt="{{$not->titulo}}" id="imgCarr" >
+                                <img src="{{ asset('storage/noticias/imagenes/'.$not->imagen) }}" alt="{{$not->titulo}}" class="d-block w-100 img-fluid rounded imgFormCarousel" >
                             </a>
                         </div>
                     @endif
