01e1b2dc85e07a01aa844d1db700b1b098ac87e4
kioselsa <30932646+kioselsa@users.noreply.github.com>
2023-12-20 11:36:34 -0600

    Actualización prorrogas

--- resources/views/content/alumnos/prorrogas.blade.php
+++ resources/views/content/alumnos/prorrogas.blade.php
@@ -7,9 +7,12 @@
 <div class="row">
     <div class="col-sm-3"></div>
     <div class="col-sm-6">
+        <h3>Instrucciones</h3>
+        <hr>
+        <a href="{{ asset('/documents/content/alumnos/GUIA PARA SOLICITAR PRORROGA 2024.pdf') }}"target="blanck" title="Formato" class="btn btn-success"><i class='fas fa-download' style='font-size:14px'></i></a>
+        <br>
         <h3>Solicitud de prorrogas</h3>
         <hr>
-        <label for="prorroga">Descarga el formato de prorroga</label>
         <a href="{{ asset('/documents/content/alumnos/solicitud de prorrogas.pdf') }}" target="blanck" title="Formato" class="btn btn-success"><i class='fas fa-download' style='font-size:14px'></i></a>
         <br>
         <br>
