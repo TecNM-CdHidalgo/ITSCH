c44e9297e3821349012d220134c636381ba7da25
Iraic <36557683+iraic@users.noreply.github.com>
2024-09-04 12:56:46 -0600

    cambio de aviso caja

--- resources/views/content/departamentos/caja.blade.php
+++ resources/views/content/departamentos/caja.blade.php
@@ -11,57 +11,43 @@
         <p style="text-align: center;"><embed class="img-fluid" src="{{asset('images/content/departamentos/servicio-caja.jpg')}}" alt="Caja" width="80%"> </p>
 
 
-
-        <p>El Instituto Tecnológico Superior de Ciudad Hidalgo no maneja efectivo por lo que los pagos para cualquier tramite se realiza por medio de deposito a la cuenta bancaria:</p>
-        <h3 class="text-center">0115142997</h3><p align="center"><strong>A nombre del Instituto Tecnológico Superior de Ciudad Hidalgo en BBVA BANCOMER</strong></p><hr class="red">
-        <h3 class="text-center">012470001151429979</h3> <p align="center"><strong>CLABE INTERBANCARIA DE BBVA BANCOMER A NOMBRE DEL INSTITUO TECNOLÓGICO SUPERIOR DE CIUDAD HIDALGO</strong></p>
-        <p>Las tarifas actualizadas al año en curso las puedes descargar desde el link:</p>
+        <h3 class="text-center">Importante</h3>
+        <p>El Instituto Tecnológico Superior de Ciudad Hidalgo no maneja efectivo por lo que los pagos para cualquier tramite se realiza a traves del Sistema de <a href="https://recibos.cdhidalgo.tecnm.mx/login">Recibos electrónicos</a></p>
+        <h3 class="text-center">Importante Aviso</h3><p align="center"><strong>Evita realizar depositos</strong></p>
+        <p align="center"><strong>A partir del 1 de septiembre de 2024, primero se debe generar la orden de pago, la cual tiene una linea de captura o numero de referencia,
+           para posteriormente, ir a pagar al banco o pagar en linea con tarjeta de debito</strong></p>
+           <hr class="red">
+           <p>Las tarifas actualizadas al año en curso las puedes descargar desde el link:</p>
 
         <p align="center">
         	<a href="{{asset('documents/content/departamentos/caja/TABULADOR 2024.pdf')}}"target="_blank"><i style="font-size:2em; padding:2px;" class="fa fa-cloud-download"></i>&nbsp;<strong>Tarifas de Servicios de Caja</strong>
         	</a>
         </p>
 
-        <p>
-          Para cambiar tu váucher bancario te invitamos a visitar el siguiente enlace, en el cual podras hacer el cambio de manera cómoda, rápida y totalmente en línea, el link es: <a href="https://recibos.cdhidalgo.tecnm.mx/login">Recibos electrónicos</a> en este mismo enlace podras encontrar un video con información útil de cómo realizar este proceso.
-        </p>
-
         <hr>
 
-        <h6>También podras realizar tu cambio de recibo en CAJA para ello te invitamos a seguir los siguientes pasos</h6>
+        <h6>También podras solicitar la orden de pago en CAJA</h6>
 
-        <p>Para poder brindarte un mejor servicio de CAJA en el cambio de tu recibo (ficha o vaucher) sigue los siguientes TIPS, de esta manera se agilizara el trámite:</p>
-
-      	<li>1.-Llenar  la ficha de depósito con los siguientes datos:
-	        <ul>
-	          <li> NOMBRE COMPLETO DEL ALUMNO </li>
-	          <li> NO. DE CONTROL (Pueden pedir en el banco se anote como referencia de pago)</li>
-	          <li> CONCEPTO DE PAGO (de acuerdo a la tarifa de servicios) </li>
-	        </ul>
+        <br>
+        <ol>
+        <li>En caso de tener prorrogas de pago, conservar  copia de las mismas para entregarlas  con la ficha de depósito al momento del cambio en caja para su respaldo.
         </li>
         <br>
-
-        <li>2.-El  depósito que  se realice por cualquier concepto ( de acuerdo a la tarifa de servicios), se puede  cambiar de manera inmediata a caja del ITSCH independientemente de las fechas que manejen otras áreas.
-        Se recomienda hacer dicho canje  con anticipación para evitar filas.
+        <li>Es importante  conservar  los recibos blancos por lo menos un año y/o de preferencia durante toda la carrera, para cualquier aclaración.
         </li>
         <br>
-        <li>3.-En caso de tener prorrogas de pago, conservar  copia de las mismas para entregarlas  con la ficha de depósito al momento del cambio en caja para su respaldo.
+        <li>Por motivos de seguridad no es posible recibir dinero en efectivo en el área de caja.
         </li>
         <br>
-        <li>4.-Es importante  conservar  los recibos blancos por lo menos un año y/o de preferencia durante toda la carrera, para cualquier aclaración.
+        <li>Es de suma importancia que no se realicen depositos bancarios directos.
         </li>
         <br>
-        <li>5.-Por motivos de seguridad no es posible recibir dinero en efectivo en el área de caja.
+        <li>En caso de realizar pagos erróneos,  tramitar devoluciones y  cambios donde se requiera cancelar algún  recibo únicamente se deberá de realizar dentro del mismo mes, (como mínimo 5 días antes de que termine el mes).
         </li>
         <br>
-        <li>6.-Es de suma importancia que si se realiza algun depósito a la cuenta del ITSCH durante el año se cambie la ficha por un recibo oficial, de ser posible de inmediato o en el mismo mes, esto con la finalidad de que al término del año no se queden sin cambiar. Puesto que es imposible identificar los depósitos y para el próximo ejercicio por motivos de cierre contable ya no se recibirá  la ficha;  ni es posible rermbolsar el pago.
+        <li>En caso de necesitar comprobante fiscal CFDI (Factura). Se debe solicitar en el área de caja al momento de pago del recibo, proporcionando los datos de facturación.
         </li>
-        <br>
-        <li>7.-En caso de realizar pagos erróneos,  tramitar devoluciones y  cambios donde se requiera cancelar algún  recibo únicamente se deberá de realizar dentro del mismo mes, (como mínimo 5 días antes de que termine el mes).
-        </li>
-        <br>
-        <li>8.-En caso de necesitar comprobante fiscal CFDI (Factura). Se debe solicitar en el área de caja al momento del canje de la ficha, proporcionando los datos de facturación.
-        </li>
+        </ol>
         <br>
 
 
