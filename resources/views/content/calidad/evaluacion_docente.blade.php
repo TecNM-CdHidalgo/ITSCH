9d9f32158b2c5e7096dc326edaf744260e030f78
Iraic <36557683+iraic@users.noreply.github.com>
2024-10-25 10:55:49 -0600

    procedmiento

--- resources/views/content/calidad/evaluacion_docente.blade.php
+++ resources/views/content/calidad/evaluacion_docente.blade.php
@@ -1,10 +1,11 @@
 <?php
 $conexion = new mysqli("localhost", "root", "W3b_L1nux", "tecnm");
 $consulta = "SELECT   * FROM profesores ORDER BY nombre_empleado ASC";
-$resultado = $conexion -> query($consulta);
+$resultado = $conexion->query($consulta);
 ?>
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -24,70 +25,83 @@ $resultado = $conexion -> query($consulta);
     <![endif]-->
 
 </head>
+
 <body>
 
-<!-- Contenido -->
-<main class="page">
-    <div class="container">
+    <!-- Contenido -->
+    <main class="page">
+        <div class="container">
 
-
-        <div class="media">
-<img src="downloads20/slide.jpg"> <img class="img-fluid" alt="Responsive image">
-        </div>
-
-        <div class="panel-group ficha-collapse" id="accordion">
-            <div class="panel panel-default">
-                <div class="panel-heading">
-                    <h4 class="panel-title">
-                        <a data-parent="#accordion" data-toggle="collapse" href="#panel-01" aria-expanded="true" aria-controls="panel-01">
-                            Resultados Febrero-Agosto 2020
-                        </a>
-                    </h4>
-                    <button type="button" class="collpase-button collapsed" data-parent="#accordion" data-toggle="collapse" href="#panel-01"></button>
+            <div class="card">
+                <div class="card-header">
+                    <h2> Procedimeinto de evaluacion docente </h2>
                 </div>
-                <div class="panel-collapse collapse in" id="panel-01">
-                    <div class="panel-body">
+                <div class="card-body">
+                    <a type="button" class="btn btn-primary"
+                        href="{{asset('documents/content/calidad/Procedimiento_evaluacion_docente.pdf')}}" data-toogle="tooltip" title="documento para descargar" download target="_blank">
+                        <i class='fas fa-download' style='font-size:20px'></i>
+                    </a>
+                </div>
+            </div>
+            <div class="media">
+                <img src="downloads20/slide.jpg"> <img class="img-fluid" alt="Responsive image">
+            </div>
+
+            <div class="panel-group ficha-collapse" id="accordion">
+                <div class="panel panel-default">
+                    <div class="panel-heading">
+                        <h4 class="panel-title">
+                            <a data-parent="#accordion" data-toggle="collapse" href="#panel-01" aria-expanded="true" aria-controls="panel-01">
+                                Resultados Febrero-Agosto 2020
+                            </a>
+                        </h4>
+                        <button type="button" class="collpase-button collapsed" data-parent="#accordion" data-toggle="collapse" href="#panel-01"></button>
+                    </div>
+                    <div class="panel-collapse collapse in" id="panel-01">
+                        <div class="panel-body">
 
 
 
 
-                        <table class="table" cellspacing="10" cellpadding="10" >
+                            <table class="table" cellspacing="10" cellpadding="10">
 
 
-                        <tr>
+                                <tr>
 
-                                <td>ID</td>
-                                <td>Nombre del Docente</td>
-                                <td>Resultados</td>
+                                    <td>ID</td>
+                                    <td>Nombre del Docente</td>
+                                    <td>Resultados</td>
 
-                            </tr>
-                            <?php      while($fila = $resultado -> fetch_array())
-                            {?>
-                            <tr>
-                                <td><?php  echo $fila["rfc"]  ?></td>
-                                <td><?php echo $fila["nombre_empleado"]; echo " " ;echo  $fila["apellidos_empleado"] ?></td>
-                                <td> <a href="<?php echo $fila["downloadlink"] ?>"> <button class="btn btn-primary" type="button">
-                                        <span class="glyphicon glyphicon-download"></span>
+                                </tr>
+                                <?php while ($fila = $resultado->fetch_array()) { ?>
+                                    <tr>
+                                        <td><?php echo $fila["rfc"]  ?></td>
+                                        <td><?php echo $fila["nombre_empleado"];
+                                            echo " ";
+                                            echo  $fila["apellidos_empleado"] ?></td>
+                                        <td> <a href="<?php echo $fila["downloadlink"] ?>"> <button class="btn btn-primary" type="button">
+                                                    <span class="glyphicon glyphicon-download"></span>
 
-                                        </button></a></td>
-                                <?php } ?>
-                            </tr>
-
-
-                        </table>
+                                                </button></a></td>
+                                    <?php } ?>
+                                    </tr>
 
 
+                            </table>
+
+
+                        </div>
                     </div>
                 </div>
             </div>
+
+
         </div>
+    </main>
 
-
-    </div>
-</main>
-
-<!-- JS -->
-<script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
+    <!-- JS -->
+    <script src="https://framework-gb.cdn.gob.mx/gobmx.js"></script>
 
 </body>
+
 </html>
