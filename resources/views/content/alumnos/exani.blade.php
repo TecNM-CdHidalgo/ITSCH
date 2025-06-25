07331b7ce4d0b1f13f91b0045306becd0a9763f4
Iraic <36557683+iraic@users.noreply.github.com>
2025-06-24 19:02:50 -0600

    examenes carreras nuevas

--- resources/views/content/alumnos/exani.blade.php
+++ resources/views/content/alumnos/exani.blade.php
@@ -315,6 +315,138 @@
         </div>
         <!-- Fin de ingeniería en nano -->
 
+
+        <!-- Ingeniería en Animacion -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/animacion.jpg') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en Animación Digital y Efectos Visuales" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en animación digital y efectos visuales </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería en animación digital y efectos visuales el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a target="_blank" href="https://forms.gle/rNxgapK31XYn5zN7A" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en Animacion -->
+
+
+        <!-- Ingeniería Agricola -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/agricola.jpg') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en Innovación Agrícola Sustentable" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en innovación agrícola sustentable</h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería en innovación agrícola sustentable el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a target="_blank" href="https://forms.gle/toD6UpNkDH9V3cmS9" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería Agricola -->
+
+
+        <!-- Ingeniería Automotrices -->
+        <div class="jumbotron text-center hoverable p-4 sombra">
+
+            <!-- Grid row -->
+            <div class="row">
+
+            <!-- Grid column -->
+            <div class="col-md-4 offset-md-1 mx-3 my-3">
+
+                <!-- Featured image -->
+                <div class="view overlay">
+                <img src="{{ asset('images/content/oferta educativa/logosCarrera/automotrices.jpg') }}" class="img-fluid" alt="Examen de nuevo ingreso de Ingeniería en Sistemas Automotrices" style="max-width: 250px">
+                <a>
+                    <div class="mask rgba-white-slight"></div>
+                </a>
+                </div>
+
+            </div>
+            <!-- Grid column -->
+
+            <!-- Grid column -->
+            <div class="col-md-7 text-md-left ml-3 mt-3">
+
+                <!-- Excerpt -->
+                <h4 class="h4 mb-4">Examen de nuevo ingreso para ingeniería en sistemas automotrices </h4>
+
+                <p class="font-weight-normal">Este examen fue desarrollado para todos los aspirantes a la ingeniería en sistemas automotrices el cual permite evaluar
+                    los conocimientos que cada uno de los candidatos posee y asi poder hacer un diagnóstico oportuno que permita hacer una caracterización de
+                    cada uno y poder tomar las medidas de apoyo necesarias para los aspirantes.
+                </p>
+                <p class="font-weight-normal">Para <a><strong>Nuevo ingreso</strong></a></p>
+
+                <a target="_blank" href="https://forms.gle/qqRbPSozWpWZxiLe9" class="btn btn-success">Ir al examen</a>
+
+            </div>
+            <!-- Grid column -->
+
+            </div>
+            <!-- Grid row -->
+
+        </div>
+        <!-- Fin de ingeniería en Automotrices -->
+
     </div>
     <div class="col-sm-1"></div>
 </div>
