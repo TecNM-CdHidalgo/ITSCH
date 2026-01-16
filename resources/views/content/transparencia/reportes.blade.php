a04718135568db10dcee34e5d26f38b31c106e2c
Iraic <joseiraic@gmail.com>
2026-01-15 18:13:41 -0600

    alineacio u079

--- resources/views/content/transparencia/reportes.blade.php
+++ resources/views/content/transparencia/reportes.blade.php
@@ -8,31 +8,31 @@
         <hr>
         <div class="row justify-content-center">
 
-            <div class="col-auto text-center mb-2">
+            <div class="col-12 text-center mb-2">
                 <a href="{{ asset('documents/content/transparencia/reportes/Oficio-DG_0758_2025-1er-Informe U079.pdf') }}" class="btn btn-outline-primary" target="_blank">
                    <i class="bi bi-download"  style="font-size:1.2em"></i>
                     &nbsp;<strong>1er Informe</strong>
                 </a>
             </div>
 
-            <div class="col-auto text-center mb-2">
+            <div class="col-12 text-center mb-2">
                 <a href="{{ asset('documents/content/transparencia/reportes/SEGUNDO-INFORME-U079.pdf') }}" class="btn btn-outline-primary" target="_blank">
                     <i class="bi bi-download"  style="font-size:1.2em"></i>
                     &nbsp;<strong>2do Informe</strong>
                 </a>
             </div>
 
-            <div class="col-auto text-center mb-2">
+            <div class="col-12 text-center mb-2">
                 <a href="{{ asset('documents/content/transparencia/reportes/Acta-Entrega-Recepcion-UO79.pdf') }}" class="btn btn-outline-primary" target="_blank">
                     <i class="bi bi-download"  style="font-size:1.2em"></i>
                     &nbsp;<strong>3er Informe</strong>
                 </a>
             </div>
 
-            <div class="col-auto text-center mb-2">
+            <div class="col-12 text-center mb-2">
                 <a href="{{ asset('documents/content/transparencia/reportes/Presupuesto-de-Egresos-Federacion-Articulo-37.pdf') }}" class="btn btn-link" target="_blank">
                     <i class="bi bi-download"  style="font-size:1.2em"></i>
-                    &nbsp;<strong>3er Informe</strong>
+                    &nbsp;<strong>Presupuesto de Egresos de la Federación Artículo 37</strong>
                 </a>
             </div>
 
